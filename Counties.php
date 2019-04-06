7e24cafa360bbec1bc22a917e8df1b4a92a9a646
stingray82 <11146912+stingray82@users.noreply.github.com>
2019-04-06 09:33:46 +0100

    Add files via upload
    This plugin is GPL released but to be sure I have only listed the modifications in the readme file and the code written by myself to enable this

---
+++ Counties.php
@@ -0,0 +1,102 @@
+<optgroup label="England">
+                              <option value="ENG-Bedfordshire"  <?php echo $state=="ENG-Bedfordshire"?"selected":""?>>Bedfordshire</option>
+                              <option value="ENG-Buckinghamshire"  <?php echo $state=="ENG-Buckinghamshire"?"selected":""?>>Buckinghamshire</option>
+                              <option value="ENG-Cambridgeshire"  <?php echo $state=="ENG-Cambridgeshire"?"selected":""?>>Cambridgeshire</option>
+                              <option value="ENG-Cheshire"  <?php echo $state=="ENG-Cheshire"?"selected":""?>>Cheshire</option>
+                              <option value="ENG-Cleveland"  <?php echo $state=="ENG-Cleveland"?"selected":""?>>Cleveland</option>
+                              <option value="ENG-Cornwall"  <?php echo $state=="ENG-Cornwall"?"selected":""?>>Cornwall</option>
+                              <option value="ENG-Cumbria"  <?php echo $state=="ENG-Cumbria"?"selected":""?>>Cumbria</option>
+                              <option value="ENG-Derbyshire"  <?php echo $state=="ENG-Derbyshire"?"selected":""?>>Derbyshire</option>
+                              <option value="ENG-Devon"  <?php echo $state=="ENG-Devon"?"selected":""?>>Devon</option>
+                              <option value="ENG-Dorset"  <?php echo $state=="ENG-Dorset"?"selected":""?>>Dorset</option>
+                              <option value="ENG-Durham"  <?php echo $state=="ENG-Durham"?"selected":""?>>Durham</option>
+                              <option value="ENG-East Sussex"  <?php echo $state=="ENG-East Sussex"?"selected":""?>>East Sussex</option>
+                              <option value="ENG-Essex"  <?php echo $state=="ENG-Essex"?"selected":""?>>Essex</option>
+                              <option value="ENG-Gloucestershire"  <?php echo $state=="ENG-Gloucestershire"?"selected":""?>>Gloucestershire</option>
+                              <option value="ENG-Greater London"  <?php echo $state=="ENG-Greater London"?"selected":""?>>Greater London</option>
+                              <option value="ENG-Greater Manchester"  <?php echo $state=="ENG-Greater Manchester"?"selected":""?>>Greater Manchester</option>
+                              <option value="ENG-Hampshire"  <?php echo $state=="ENG-Hampshire"?"selected":""?>>Hampshire</option>
+                              <option value="ENG-Hertfordshire"  <?php echo $state=="ENG-Hertfordshire"?"selected":""?>>Hertfordshire</option>
+                              <option value="ENG-Kent"  <?php echo $state=="ENG-Kent"?"selected":""?>>Kent</option>
+                              <option value="ENG-Lancashire"  <?php echo $state=="ENG-Lancashire"?"selected":""?>>Lancashire</option>
+                              <option value="ENG-Leicestershire"  <?php echo $state=="ENG-Leicestershire"?"selected":""?>>Leicestershire</option>
+                              <option value="ENG-Lincolnshire"  <?php echo $state=="ENG-Lincolnshire"?"selected":""?>>Lincolnshire</option>
+                              <option value="ENG-Merseyside"  <?php echo $state=="ENG-Merseyside"?"selected":""?>>Merseyside</option>
+                              <option value="ENG-Norfolk"  <?php echo $state=="ENG-Norfolk"?"selected":""?>>Norfolk</option>
+                              <option value="ENG-North Yorkshire"  <?php echo $state=="ENG-North Yorkshire"?"selected":""?>>North Yorkshire</option>
+                              <option value="ENG-Northamptonshire"  <?php echo $state=="ENG-Northamptonshire"?"selected":""?>>Northamptonshire</option>
+                              <option value="ENG-Northumberland"  <?php echo $state=="ENG-Northumberland"?"selected":""?>>Northumberland</option>
+                              <option value="ENG-Nottinghamshire"  <?php echo $state=="ENG-Nottinghamshire"?"selected":""?>>Nottinghamshire</option>
+                              <option value="ENG-Oxfordshire"  <?php echo $state=="ENG-Oxfordshire"?"selected":""?>>Oxfordshire</option>
+                              <option value="ENG-Shropshire"  <?php echo $state=="ENG-Shropshire"?"selected":""?>>Shropshire</option>
+                              <option value="ENG-Somerset"  <?php echo $state=="ENG-Somerset"?"selected":""?>>Somerset</option>
+                              <option value="ENG-South Yorkshire"  <?php echo $state=="ENG- South Yorkshire"?"selected":""?>>South Yorkshire</option>
+                              <option value="ENG-Staffordshire"  <?php echo $state=="ENG-Staffordshire"?"selected":""?>>Staffordshire</option>
+                              <option value="ENG-Suffolk"  <?php echo $state=="ENG-Suffolk"?"selected":""?>>Suffolk</option>
+                              <option value="ENG-Surrey"  <?php echo $state=="ENG-Surrey"?"selected":""?>>Surrey</option>
+                              <option value="ENG-Tyne and Wear"  <?php echo $state=="ENG-Tyne and Wear"?"selected":""?>>Tyne and Wear</option>
+                              <option value="ENG-Warwickshire"  <?php echo $state=="ENG-Warwickshire"?"selected":""?>>Warwickshire</option>
+                              <option value="ENG-West Berkshire"  <?php echo $state=="ENG-West Berkshire"?"selected":""?>>West Berkshire</option>
+                              <option value="ENG-West Midlands"  <?php echo $state=="ENG-West Midlands"?"selected":""?>>West Midlands</option>
+                              <option value="ENG-West Sussex"  <?php echo $state=="ENG-West Sussex"?"selected":""?>>West Sussex</option>
+                              <option value="ENG-West Yorkshire"  <?php echo $state=="ENG-West Yorkshire"?"selected":""?>>West Yorkshire</option>
+                              <option value="ENG-Wiltshire"  <?php echo $state=="ENG-Wiltshire"?"selected":""?>>Wiltshire</option>
+                              <option value="ENG-Worcestershire"  <?php echo $state=="ENG-Worcestershire"?"selected":""?>>Worcestershire</option>                      
+                          </optgroup>
+                          <optgroup label="Scotland">
+                              <option value="SCO-Aberdeen City"  <?php echo $state=="SCO-Aberdeen City"?"selected":""?>>Aberdeen City</option>
+                              <option value="SCO-Aberdeenshire"  <?php echo $state=="SCO-Aberdeenshire"?"selected":""?>>Aberdeenshire</option>
+                              <option value="SCO-Angus"  <?php echo $state=="SCO-Angus"?"selected":""?>>Angus</option>
+                              <option value="SCO-Argyll and Bute"  <?php echo $state=="SCO-Argyll and Bute"?"selected":""?>>Argyll and Bute</option>
+                              <option value="SCO-City of Edinburgh"  <?php echo $state=="SCO-City of Edinburgh"?"selected":""?>>City of Edinburgh</option>
+                              <option value="SCO-Clackmannanshire"  <?php echo $state=="SCO-Clackmannanshire"?"selected":""?>>Clackmannanshire</option>
+                              <option value="SCO-Dumfries and Galloway"  <?php echo $state=="SCO-Dumfries and Galloway"?"selected":""?>>Dumfries and Galloway</option>
+                              <option value="SCO-Dundee City"  <?php echo $state=="SCO-Dundee City"?"selected":""?>>Dundee City</option>
+                              <option value="SCO-East Ayrshire"  <?php echo $state=="SCO-East Ayrshire"?"selected":""?>>East Ayrshire</option>
+                              <option value="SCO-East Dunbartonshire"  <?php echo $state=="SCO-East Dunbartonshire"?"selected":""?>>East Dunbartonshire</option>
+                              <option value="SCO-East Lothian"  <?php echo $state=="SCO-East Lothian"?"selected":""?>>East Lothian</option>
+                              <option value="SCO-East Renfrewshire"  <?php echo $state=="SCO-East Renfrewshire"?"selected":""?>>East Renfrewshire</option>
+                              <option value="SCO-Eilean Siar"  <?php echo $state=="SCO-Eilean Siar"?"selected":""?>>Eilean Siar</option>
+                              <option value="SCO-Falkirk"  <?php echo $state=="SCO-Falkirk"?"selected":""?>>Falkirk</option>
+                              <option value="SCO-Fife"  <?php echo $state=="SCO-Fife"?"selected":""?>>Fife</option>
+                              <option value="SCO-Glasgow City"  <?php echo $state=="SCO-Glasgow City"?"selected":""?>>Glasgow City</option>
+                              <option value="SCO-Highland"  <?php echo $state=="SCO-Highland"?"selected":""?>>Highland</option>
+                              <option value="SCO-Inverclyde"  <?php echo $state=="SCO-Inverclyde"?"selected":""?>>Inverclyde</option>
+                              <option value="SCO-Midlothian"  <?php echo $state=="SCO-Midlothian"?"selected":""?>>Midlothian</option>
+                              <option value="SCO-Moray"  <?php echo $state=="SCO-Moray"?"selected":""?>>Moray</option>
+                              <option value="SCO-North Ayrshire"  <?php echo $state=="SCO-North Ayrshire"?"selected":""?>>North Ayrshire</option>
+                              <option value="SCO-North Lanarkshire"  <?php echo $state=="SCO-North Lanarkshire"?"selected":""?>>North Lanarkshire</option>
+                              <option value="SCO-Orkney Islands"  <?php echo $state=="SCO-Orkney Islands"?"selected":""?>>Orkney Islands<option>
+                              <option value="SCO-Perth and Kinross"  <?php echo $state=="SCO-Perth and Kinross"?"selected":""?>>Perth and Kinross</option>
+                              <option value="SCO-Renfrewshire"  <?php echo $state=="SCO-Renfrewshire"?"selected":""?>>Renfrewshire</option>
+                              <option value="SCO-Scottish Borders"  <?php echo $state=="SCO-Scottish Borders"?"selected":""?>>Scottish Borders</option>
+                              <option value="SCO-Shetland Islands"  <?php echo $state=="SCO-Shetland Islands"?"selected":""?>>Shetland Islands</option>
+                              <option value="SCO-South Ayrshire"  <?php echo $state=="SCO-South Ayrshire"?"selected":""?>>South Ayrshire</option>
+                              <option value="SCO-South Lanarkshire"  <?php echo $state=="SCO-South Lanarkshire"?"selected":""?>>South Lanarkshire</option>
+                              <option value="SCO-Stirling"  <?php echo $state=="SCO-Stirling"?"selected":""?>>Stirling</option>                 
+                              <option value="SCO-West Dunbartonshire"  <?php echo $state=="SCO-West Dunbartonshire"?"selected":""?>>West Dunbartonshire</option>            
+                              <option value="SCO-West Lothian"  <?php echo $state=="SCO-West Lothian"?"selected":""?>>West Lothian</option>                 
+                          </optgroup>
+                          <optgroup label="Wales">                              
+                              <option value="WAL-Anglesey"  <?php echo $state=="WAL-Anglesey"?"selected":""?>>Anglesey</option>
+                              <option value="WAL-Breconshire"  <?php echo $state=="WAL-Breconshire"?"selected":""?>>Breconshire</option>
+                              <option value="WAL-Cardiganshire"  <?php echo $state=="WAL-Cardiganshire"?"selected":""?>>Cardiganshire</option>
+                              <option value="WAL-Caernarvonshire"  <?php echo $state=="WAL-Caernarvonshire"?"selected":""?>>Caernarvonshire</option>
+                              <option value="WAL-Carmarthenshire"  <?php echo $state=="WAL-Carmarthenshire"?"selected":""?>>Carmarthenshire</option>
+                              <option value="WAL-Denbighshire"  <?php echo $state=="WAL-Denbighshire"?"selected":""?>>Denbighshire</option>
+                              <option value="WAL-Flintshire"  <?php echo $state=="WAL-Flintshire"?"selected":""?>>Flintshire</option>
+                              <option value="WAL-Glamorgan"  <?php echo $state=="WAL-Glamorgan"?"selected":""?>>Glamorgan</option>
+                              <option value="WAL-Merionethshire"  <?php echo $state=="WAL-Merionethshire"?"selected":""?>>Merionethshire</option>
+                              <option value="WAL-Monmouthshire"  <?php echo $state=="WAL-Monmouthshire"?"selected":""?>>Monmouthshire</option>
+                              <option value="WAL-Montgomeryshire"  <?php echo $state=="WAL-Montgomeryshire"?"selected":""?>>Montgomeryshire</option>
+                              <option value="WAL-Pembrokeshire"  <?php echo $state=="WAL-Pembrokeshire"?"selected":""?>>Pembrokeshire</option>
+                              <option value="WAL-Radnorshire"  <?php echo $state=="WAL-Radnorshire"?"selected":""?>>Radnorshire</option>
+                          </optgroup>
+                         <optgroup label="Northern Ireland">
+                              <option value="-NI-Antrim"  <?php echo $state=="-NI-Antrim"?"selected":""?>>Antrim</option>
+                              <option value="-NI-Armagh"  <?php echo $state=="-NI-Armagh"?"selected":""?>>Armagh</option>
+                              <option value="-NI-Down"    <?php echo $state=="-NI-Down"?"selected":""?>>Down</option>
+                              <option value="-NI-Fermanagh"  <?php echo $state=="-NI-Fermanagh"?"selected":""?>>Fermanagh</option>
+                              <option value="-NI-Derry-Londonderry"  <?php echo $state=="-NI-Derry-Londonderry"?"selected":""?>>Derry and Londonderry</option>
+                              <option value="-NI-Tyrone"  <?php echo $state=="-NI-Tyrone"?"selected":""?>>Tyrone</option>
+                          </optgroup>
